ab9b2a47a0491e51bc7dc9a5078b485e0a957805
icarus523 <icarus523@users.noreply.github.com>
2018-09-14 12:03:58 +1000

    Changes to allow OLGR testing
    Change of blob1Name and blob2Name to allow script to run (names are already used) 
    Change Service Bus name, to allow script to run (name already in use)

--- prod-qolgr/deploy_prod_paas.ps1
+++ prod-qolgr/deploy_prod_paas.ps1
@@ -1,4 +1,9 @@
+<#
+Previous subscriptionId: 
 $subscriptionId = "6401edaa-7d66-458e-9cac-04389b15bace"
+
+#>
+$subscriptionId = "bb1dd1e3-b731-4239-a1a0-fa1a52457b46"
 $resourceGroupName = "PROD-DVSOLGR-paas"
 $resourceGroupLocation = "Australia East"
 $keyVaultName = "DVSOLGR-PROD-keyvault"
@@ -16,9 +21,9 @@ $busParametersFilePath = "sb-prd.json"
 ..\_common\azure_create_keyvault.ps1 $resourceGroupName $resourceGroupLocation $keyVaultName
 
 # These must match the values in the blob parameter files
-$blob1Name = "dvsolgrblobprd"
-$blob2Name = "dvsolgrblob2prd"
-$serviceBusName = "dvsolgrsbprd"
+$blob1Name = "dvsolgrblobprdv2"
+$blob2Name = "dvsolgrblob2prdv2"
+$serviceBusName = "dvsolgrsbprdv2"
 ..\_common\enable_azurestorage_diagnostics.ps1 $resourceGroupName $blob1Name
 ..\_common\enable_azurestorage_diagnostics.ps1 $resourceGroupName $blob2Name
 ..\_common\get_paas_connection_strings.ps1 $resourceGroupName $blob1Name $blob2Name $serviceBusName
