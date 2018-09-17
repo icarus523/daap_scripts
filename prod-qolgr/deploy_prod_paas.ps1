027f996d322bf6892755417c7982f17a0c65206c
icarus523 <icarus523@users.noreply.github.com>
2018-09-17 11:50:05 +1000

    Committing changes for PaaS working.
    Added ability t remove AzureRmADApplication
    Comments on blob and service bus name parameters 
    Deployed Keyvault name must change.

--- prod-qolgr/deploy_prod_paas.ps1
+++ prod-qolgr/deploy_prod_paas.ps1
@@ -1,12 +1,13 @@
-<#
-Previous subscriptionId: 
-$subscriptionId = "6401edaa-7d66-458e-9cac-04389b15bace"
 
-#>
+# JA Previous subscriptionId: 
+#$subscriptionId = "6401edaa-7d66-458e-9cac-04389b15bace"
+
+# DJAG subscriptionId
 $subscriptionId = "bb1dd1e3-b731-4239-a1a0-fa1a52457b46"
 $resourceGroupName = "PROD-DVSOLGR-paas"
 $resourceGroupLocation = "Australia East"
-$keyVaultName = "DVSOLGR-PROD-keyvault"
+# JA keyVaultName has to be changed, as the original name is being used, and I cannot see it (to remove it) 
+$keyVaultName = "DVSOLGR-PROD-keyvault2"
 $blobTemplateFilePath = "..\_common\template_storageblob.json"
 $busTemplateFilePath = "..\_common\template_servicebus.json"
 
@@ -20,13 +21,14 @@ $busParametersFilePath = "sb-prd.json"
 ..\_common\azure_deploy_template.ps1 $subscriptionId $resourceGroupName $resourceGroupLocation $resourceGroupName $busTemplateFilePath $busParametersFilePath
 ..\_common\azure_create_keyvault.ps1 $resourceGroupName $resourceGroupLocation $keyVaultName
 
-# Tag resources
-Set-AzureRmResourceGroup -Name $resourceGroupName ​-Tag @{Department="DJAG";businessowner="OLGR";"Business Unit"="OLGR";application="TATTS Lotto Verification";Environment="DEVEL";infoclassification="XXXXX"}​
+# JA Tag resources
+Set-AzureRmResourceGroup -Name $resourceGroupName -Tag @{Department="DJAG";businessowner="OLGR";"Business Unit"="OLGR";application="TATTS Lotto Verification";Environment="DEVEL";infoclassification="XXXXX"}
 
 # These must match the values in the blob parameter files
-$blob1Name = "dvsolgrblobprdv2"
-$blob2Name = "dvsolgrblob2prdv2"
-$serviceBusName = "dvsolgrsbprdv2"
+# JA These have also been changed, as the script will error out indicating invalid template. 
+$blob1Name = "dvsolgrblobprd2"
+$blob2Name = "dvsolgrblob2prd2"
+$serviceBusName = "dvsolgrsbprd2"
 ..\_common\enable_azurestorage_diagnostics.ps1 $resourceGroupName $blob1Name
 ..\_common\enable_azurestorage_diagnostics.ps1 $resourceGroupName $blob2Name
 ..\_common\get_paas_connection_strings.ps1 $resourceGroupName $blob1Name $blob2Name $serviceBusName
--- prod-qolgr/teardown_paas_prod.ps1
+++ prod-qolgr/teardown_paas_prod.ps1
@@ -1,3 +1,11 @@
 ..\_common\azure_verify_login.ps1
 
 Remove-AzureRmResourceGroup -Name PROD-DVSOLGR-paas
+
+# Manually remove the AzureRmADApplication (should this be done)
+$appDisplayName = "DVS.Server"
+$azureAdApplication_list = Get-AzureRmADApplication -DisplayNameStartWith $appDisplayName
+
+foreach ($azureAdApp in $azureAdApplication_list) {
+    Remove-AzureRmADApplication -ApplicationId $azureAdApp.ApplicationId
+}
