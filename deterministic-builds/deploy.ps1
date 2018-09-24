817152f0135f87f1452a1bdb5c722d09cabbabfc
icarus523 <icarus523@users.noreply.github.com>
2018-09-24 10:08:44 +1000

    Adding support for latest VLAN required changes

--- deterministic-builds/deploy.ps1
+++ deterministic-builds/deploy.ps1
@@ -1,8 +1,5 @@
-<#
-Previous subscriptionId: 
-$subscriptionId = "6401edaa-7d66-458e-9cac-04389b15bace"
-
-#>
+# JA Previous subscriptionId: 
+# $subscriptionId = "6401edaa-7d66-458e-9cac-04389b15bace"
 $subscriptionId = "bb1dd1e3-b731-4239-a1a0-fa1a52457b46"
 $resourceGroupName = "DeterministicBuilds"
 $resourceGroupLocation = "Australia East"
@@ -14,8 +11,8 @@ $vmParametersFilePath = "parameters.json"
 ..\_common\azure_verify_login.ps1
 ..\_common\azure_deploy_template.ps1 $subscriptionId $resourceGroupName $resourceGroupLocation $deploymentName
 
-# Tag resources
-Set-AzureRmResourceGroup -Name $resourceGroupName ​-Tag @{Department="DJAG";businessowner="OLGR";"Business Unit"="OLGR";application="TATTS Lotto Verification";Environment="DEVEL";infoclassification="XXXXX"}​
+# JA Tag resources
+Set-AzureRmResourceGroup -Name $resourceGroupName -Tag @{Department="DJAG";businessowner="OLGR";"Business Unit"="OLGR";application="TATTS Lotto Verification";Environment="DEVEL";infoclassification="XXXXX"}
 
 $rdpFilePath = "$PSScriptRoot\build_machine.rdp"
 Get-AzureRmRemoteDesktopFile -ResourceGroupName $resourceGroupName -Name "build" -LocalPath $rdpFilePath -Launch
